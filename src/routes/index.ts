ffd67159ec889b5bf28e1145a0a013408556ba77
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-03-07 10:55:22 -0300

    Aula Programação de Scripts - 7 de Março de 2023

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import data from './data';
 import matematica from './matematica';
+import Login from '../controllers/Login';
 
 const routes = Router();
 
 routes.use('/date', data);
 routes.use('/math', matematica);
+routes.post('/login', Login.login);
 
 export default routes;
